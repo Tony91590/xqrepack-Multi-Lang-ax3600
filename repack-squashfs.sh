2e959b91e35527f5419c3cfdd2bd76e20cd5ce47
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-10-16 01:31:15 +0200

    Update repack-squashfs.sh

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -49,7 +49,7 @@ for SVC in stat_points statisticsservice \
 done
 
 # prevent stats phone home & auto-update
-for f in StatPoints mtd_crash_log logupload.lua otapredownload wanip_check.sh; do > $FSDIR/usr/sbin/$f; done
+for f in StatPoints mtd_crash_log logupload.lua otapredownload; do > $FSDIR/usr/sbin/$f; done
 
 sed -i '/start_service(/a return 0' $FSDIR/etc/init.d/messagingagent.sh
 
