0fc40bc51d80c33c25a239026afb8a91ae98687a
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-10-16 02:57:37 +0200

    Update qcawificfg80211.sh

--- lib/wifi/qcawificfg80211.sh
+++ lib/wifi/qcawificfg80211.sh
@@ -4303,10 +4303,10 @@ enable_vifs_qcawificfg80211() {
 			iwpriv "$ifname" vhtsubfee 0
 			iwpriv "$ifname" he_subfee 0
 		else
-			if [ "$channel" -lt 100 ]; then
-				max_power=23
-			else
+			if [ "$channel" -le 100 ]; then
 				max_power=24
+			else
+				max_power=23
 			fi
 		fi
 
