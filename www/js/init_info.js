46d28c95e5fd85eb8aa0f7ce0b0d8fdff4098bc2
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2023-11-22 17:37:07 +0100

    go

---
+++ www/js/init_info.js
@@ -0,0 +1,1 @@
+$.ajax({url:window.location.origin+"/cgi-bin/luci/api/xqsystem/init_info",type:"GET",dataType:"json",success:function(rsp){if(rsp.code===0){if(rsp.displayName){$(".name").text(rsp.displayName);$(".hardware").text(rsp.hardware);$(".romversion").text(rsp.romversion)}}else{$.alert(rsp.msg)}}});
