8a0a5dec9b1cdb618a938eafbb46b93d0e568726
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2024-01-08 06:07:39 +0100

    Update repack-squashfs.sh

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -30,81 +30,46 @@ unsquashfs -f -d "$FSDIR" "$IMG"
 >&2 echo "patching squashfs..."
 
 # modify dropbear init
-sed -i 's/channel=.*/channel=release2/' "$FSDIR/etc/init.d/dropbear"
-sed -i 's/flg_ssh=.*/flg_ssh=1/' "$FSDIR/etc/init.d/dropbear"
-
-# mark web footer so that users can confirm the right version has been flashed
-sed -i 's/romVersion%>/& xqrepack/;' "$FSDIR/usr/lib/lua/luci/view/web/inc/footer.htm"
+sed -i 's/channel=.*/channel=release2/' "/etc/init.d/dropbear"
+sed -i 's/flg_ssh=.*/flg_ssh=1/' "/etc/init.d/dropbear"
 
 # stop resetting root password
-sed -i '/set_user(/a return 0' "$FSDIR/etc/init.d/system"
-sed -i 's/flg_init_pwd=.*/flg_init_pwd=0/' "$FSDIR/etc/init.d/boot_check"
+sed -i '/set_user(/a return 0' "/etc/init.d/system"
+sed -i 's/flg_init_pwd=.*/flg_init_pwd=0/' "/etc/init.d/boot_check"
 
-# make sure our backdoors are always enabled by default
-sed -i '/ssh_en/d;' "$FSDIR/usr/share/xiaoqiang/xiaoqiang-reserved.txt"
-sed -i '/ssh_en=/d; /uart_en=/d; /boot_wait=/d;' "$FSDIR/usr/share/xiaoqiang/xiaoqiang-defaults.txt"
-cat <<XQDEF >> "$FSDIR/usr/share/xiaoqiang/xiaoqiang-defaults.txt"
-uart_en=1
-ssh_en=1
-boot_wait=on
-XQDEF
-
-# always reset our access nvram variables
-grep -q -w enable_dev_access "$FSDIR/lib/preinit/31_restore_nvram" || \
- cat <<NVRAM >> "$FSDIR/lib/preinit/31_restore_nvram"
-enable_dev_access() {
-	nvram set uart_en=1
-	nvram set ssh_en=1
-	nvram set boot_wait=on
-	nvram commit
-}
-
-boot_hook_add preinit_main enable_dev_access
-NVRAM
-
-# modify root password
-sed -i "s@root:[^:]*@root:${ROOTPW}@" "$FSDIR/etc/shadow"
 
 # stop phone-home in web UI
-cat <<JS >> "$FSDIR/www/js/miwifi-monitor.js"
+cat <<JS >> "/www/js/miwifi-monitor.js"
 (function(){ if (typeof window.MIWIFI_MONITOR !== "undefined") window.MIWIFI_MONITOR.log = function(a,b) {}; })();
 JS
 
-# add xqflash tool into firmware for easy upgrades
-cp xqflash "$FSDIR/sbin"
-chmod 0755      "$FSDIR/sbin/xqflash"
-chown root:root "$FSDIR/sbin/xqflash"
+
 
 # dont start crap services
 for SVC in stat_points statisticsservice \
 		datacenter \
 		smartcontroller \
 		plugincenter plugin_start_script.sh cp_preinstall_plugins.sh; do
-	rm -f $FSDIR/etc/rc.d/[SK]*$SVC
+	rm -f /etc/rc.d/[SK]*$SVC
 done
 
 # prevent stats phone home & auto-update
-for f in StatPoints mtd_crash_log logupload.lua otapredownload wanip_check.sh; do > $FSDIR/usr/sbin/$f; done
+for f in StatPoints mtd_crash_log logupload.lua otapredownload wanip_check.sh; do > /usr/sbin/$f; done
 
-rm -f $FSDIR/etc/hotplug.d/iface/*wanip_check
+rm -f /etc/hotplug.d/iface/*wanip_check
 
 for f in wan_check messagingagent.sh; do
-	sed -i '/start_service(/a return 0' $FSDIR/etc/init.d/$f
+	sed -i '/start_service(/a return 0' /etc/init.d/$f
 done
 
 # cron jobs are mostly non-OpenWRT stuff
-for f in $FSDIR/etc/crontabs/*; do
+for f in /etc/crontabs/*; do
 	sed -i 's/^/#/' $f
 done
 
 # as a last-ditch effort, change the *.miwifi.com hostnames to localhost
-sed -i 's@\w\+.miwifi.com@localhost@g' $FSDIR/etc/config/miwifi
+sed -i 's@\w\+.miwifi.com@localhost@g' /etc/config/miwifi
 
-# apply patch from xqrepack repository
-find patches -type f -exec bash -c "(cd "$FSDIR" && patch -p1) < {}" \;
-find patches -type f -name \*.orig -delete
-
-rm -f $FSDIR/etc/config/xqled.orig
 
 >&2 echo "repacking squashfs..."
 rm -f "$IMG.new"
