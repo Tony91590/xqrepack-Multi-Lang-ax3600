73424fee92b8ec78febbb7da7ccac0abd7674ab0
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2024-01-12 18:25:20 +0100

    Update repack-squashfs.sh

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -39,15 +39,14 @@ sed -i 's/flg_init_pwd=.*/flg_init_pwd=0/' "/etc/init.d/boot_check"
 
 cp -R lib/* "$FSDIR/lib/"
 
-sed -i 's/149/132/' "$FSDIR/usr/lib/lua/luci/view/web/setting/wifi.htm"
-sed -i 's/149/132/' "$FSDIR/usr/lib/lua/luci/view/web/apsetting/wifi.htm"
-sed -i 's/149/132/' "$FSDIR/usr/lib/lua/luci/view/web/inc/wifi.html"
+#sed -i 's/149/132/' "$FSDIR/usr/lib/lua/luci/view/web/setting/wifi.htm"
+#sed -i 's/149/132/' "$FSDIR/usr/lib/lua/luci/view/web/apsetting/wifi.htm"
+#sed -i 's/149/132/' "$FSDIR/usr/lib/lua/luci/view/web/inc/wifi.html"
 
 # apply patch from xqrepack repository
 find patches -type f -exec bash -c "(cd "$FSDIR" && patch -p1) < {}" \;
 find patches -type f -name \*.orig -delete
 
-rm -f $FSDIR/etc/config/xqled.orig
 rm -f $FSDIR/lib/wifi/qcawificfg80211.sh.orig
 rm -f $FSDIR/usr/lib/lua/luci/view/web/apsetting/wifi.htm.orig
 rm -f $FSDIR/usr/lib/lua/luci/view/web/inc/wifi.html.orig
