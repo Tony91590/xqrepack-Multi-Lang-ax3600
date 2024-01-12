15011e8ee4fabb2b2faccb01081c5fb45e83103d
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2024-01-12 18:24:54 +0100

    Update repack-squashfs.sh

--- repack-squashfs.sh
+++ repack-squashfs.sh
@@ -37,6 +37,22 @@ sed -i 's/flg_ssh=.*/flg_ssh=1/' "/etc/init.d/dropbear"
 sed -i '/set_user(/a return 0' "/etc/init.d/system"
 sed -i 's/flg_init_pwd=.*/flg_init_pwd=0/' "/etc/init.d/boot_check"
 
+cp -R lib/* "$FSDIR/lib/"
+
+sed -i 's/149/132/' "$FSDIR/usr/lib/lua/luci/view/web/setting/wifi.htm"
+sed -i 's/149/132/' "$FSDIR/usr/lib/lua/luci/view/web/apsetting/wifi.htm"
+sed -i 's/149/132/' "$FSDIR/usr/lib/lua/luci/view/web/inc/wifi.html"
+
+# apply patch from xqrepack repository
+find patches -type f -exec bash -c "(cd "$FSDIR" && patch -p1) < {}" \;
+find patches -type f -name \*.orig -delete
+
+rm -f $FSDIR/etc/config/xqled.orig
+rm -f $FSDIR/lib/wifi/qcawificfg80211.sh.orig
+rm -f $FSDIR/usr/lib/lua/luci/view/web/apsetting/wifi.htm.orig
+rm -f $FSDIR/usr/lib/lua/luci/view/web/inc/wifi.html.orig
+rm -f $FSDIR/usr/lib/lua/luci/view/web/setting/wifi.htm.orig
+
 >&2 echo "repacking squashfs..."
 rm -f "$IMG.new"
 mksquashfs "$FSDIR" "$IMG.new" -comp xz -b 256K -no-xattrs
